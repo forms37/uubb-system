5e308c50b36b0886ade33b1f0f8d77f2cce0e24b
forms37 <ml.apoyo.web@gmail.com>
2026-01-15 12:36:20 -0500

    Enhance build.sh to create superuser if absent
    Add automatic superuser creation to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput || true
 python manage.py migrate
+
+# Crear superusuario automáticamente (si no existe)
+python manage.py createsuperuser --noinput || true
